b8132c1bd27d5477b8c64685ff56a1d87aea5e3b
rbrtdambrosio <rbrtdambrosio@gmail.com>
2025-02-24 20:46:13 +0100

    new map test

--- src/new_game.c
+++ src/new_game.c
@@ -127,7 +127,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(TESTGiardinoAlice), MAP_NUM(TESTGiardinoAlice), -1, 9, 11);
     WarpIntoMap();
 }
 
